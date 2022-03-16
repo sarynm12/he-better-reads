9016de10a68073f7e0770de5bfdc4788aa75ebe4
Saryn Mooney <smooney15@my.whitworth.edu>
2022-03-16 09:29:50 -0600

    add test for index action

--- spec/requests/api/reviews_spec.rb
+++ spec/requests/api/reviews_spec.rb
@@ -1,3 +1,25 @@
 RSpec.describe '/api/reviews' do
+  let(:response_hash) { JSON(response.body, symbolize_names: true) }
 
+  describe 'GET to /' do
+    it 'returns all reviews' do
+      review = create(:review)
+
+      get api_reviews_path
+
+      expect(response_hash).to eq(
+        [
+          {
+            book_id: review.book_id,
+            user_id: review.user_id,
+            created_at: review.created_at.iso8601(3),
+            rating: review.rating,
+            id: review.id,
+            description: review.description,
+            updated_at: review.updated_at.iso8601(3)
+          }
+        ]
+      )
+    end
+  end
 end
