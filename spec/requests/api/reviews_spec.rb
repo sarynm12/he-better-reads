3b8d75f715c6c331a5c34514ed42654eb4aaad3e
Saryn Mooney <smooney15@my.whitworth.edu>
2022-03-16 09:58:05 -0600

    add to index action test

--- spec/requests/api/reviews_spec.rb
+++ spec/requests/api/reviews_spec.rb
@@ -3,20 +3,30 @@ RSpec.describe '/api/reviews' do
 
   describe 'GET to /' do
     it 'returns all reviews' do
-      review = create(:review)
+      review = create_list(:review, 2)
 
       get api_reviews_path
 
+      expect(Review.count).to eq(2)
       expect(response_hash).to eq(
         [
           {
-            book_id: review.book_id,
-            user_id: review.user_id,
-            created_at: review.created_at.iso8601(3),
-            rating: review.rating,
-            id: review.id,
-            description: review.description,
-            updated_at: review.updated_at.iso8601(3)
+            book_id: review[0].book_id,
+            user_id: review[0].user_id,
+            created_at: review[0].created_at.iso8601(3),
+            rating: review[0].rating,
+            id: review[0].id,
+            description: review[0].description,
+            updated_at: review[0].updated_at.iso8601(3)
+          },
+          {
+            book_id: review[1].book_id,
+            user_id: review[1].user_id,
+            created_at: review[1].created_at.iso8601(3),
+            rating: review[1].rating,
+            id: review[1].id,
+            description: review[1].description,
+            updated_at: review[1].updated_at.iso8601(3)
           }
         ]
       )
