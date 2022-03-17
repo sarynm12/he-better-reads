2a82ab29449989745d33d824d87780c3ab025264
Saryn Mooney <smooney15@my.whitworth.edu>
2022-03-17 15:21:39 -0600

    add route for a book's reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :authors, only: [:create, :index, :show, :update]
-    resources :books, only: [:create, :index, :show, :update]
+    resources :books, only: [:create, :index, :show, :update] do
+      get '/reviews', to: 'books/reviews#index'
+    end
     resources :users, only: [:create, :index, :show, :update]
     resources :reviews, only: [:create, :index, :show, :update]
   end
